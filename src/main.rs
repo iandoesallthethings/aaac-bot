ef71e8820dbfde3ade8a2ae69d98e3eceb746c30
iandoesallthethings <iandoesallthethings@gmail.com>
2023-07-06 22:25:17 +0200

    Add a comment about deploying to vercel.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use chrono::prelude::*;
 mod ical;
 use ical::*;
 
+// Deploying to vercel
+// https://github.com/vercel-community/rust
+
 fn main() {
     let date = Local::now().format("%Y-%m-%d").to_string();
     let url = format!("https://flagpole.com/?post_type=tribe_events&ical=1&tribe-bar-date={date}");
